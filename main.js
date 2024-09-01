7c8fc900af78d4ef9cbb75e62cb579bcc1ce64dc
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-09-01 16:28:58 +0530

    fetch users

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const PAGE_SIZE = 10;
+const api_url = 'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
+let users = [];
 
+async function fetchUsers() {
+  try {
+    //get users from api
+    const response = await fetch(api_url);
+    users = await response.json();
+    console.log(users);
+  } catch (error) {
+    console.log('Error fetching users:', users);
+  }
+}
+
+fetchUsers();
