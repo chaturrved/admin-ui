deb361193908455e2ad5e4f95f8e7a5bc42675a1
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-09-01 17:20:47 +0530

    pagination

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ async function fetchUsers() {
     users = await response.json();
     filteredUsers = [...users];
     renderTable();
+    renderPaginationButtons();
     console.log(users);
   } catch (error) {
     console.log('Error fetching users:', users);
@@ -37,7 +38,7 @@ function renderTable() {
         <td>
           <button class="edit" onclick="handleEdit('${user.id}')">Edit</button>
           <button class="delete" onclick="handleDelete('${user.id}')">Delete</button>
-        </td> 
+        </td>
     `;
 
     if (selectedRows.includes(user.id)) {
@@ -47,5 +48,61 @@ function renderTable() {
     tableBody.appendChild(row);
   })
 }
+
+function renderPaginationButtons() {
+  const totalPages = Math.ceil(filteredUsers.length / PAGE_SIZE);
+  const pageNumbers = document.getElementById('pageNumbers');
+  pageNumbers.innerHTML = '';
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement('button');
+    button.textContent = i;
+    button.onclick = () => changePage(i);
+    if (i === currentPage) {
+      button.disabled = true;
+    }
+    pageNumbers.appendChild(button);
+  }
+}
+
+function changePage(action) {
+  const totalPages = Math.ceil(filteredUsers.length / PAGE_SIZE);
+  switch (action) {
+    case 'first':
+      currentPage = 1;
+      break;
+    case 'previous':
+      currentPage = Math.max(1, currentPage - 1);
+      break;
+    case 'next':
+      currentPage = Math.min(totalPages, currentPage + 1);
+      break;
+    case 'last':
+      currentPage = totalPages;
+      break;
+    default:
+      currentPage = action;
+  }
+
+  renderTable();
+  renderPaginationButtons();
+}
+
+function handleRowSelect(id) {
+  if (selectedRows.includes(id)) {
+    selectedRows = selectedRows.filter(rowId => rowId !== id);
+  } else {
+    selectedRows.push(id);
+  }
+  renderTable();
+}
+
+function handleEdit(id) {
+};
+
+function handleDelete(id) {
+};
+
+//To Make functions available globally for inline html ex: onclick
+window.changePage = changePage;
 
 fetchUsers();
